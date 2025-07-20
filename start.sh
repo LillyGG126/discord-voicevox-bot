8825cd3f6ae6d70a80083f2042c8d76a00a53897
LillyGG126 <riverslily0126.x@gmail.com>
2025-07-20 14:12:40 +0900

    Fix VOICEVOX Engine path and prepare for Background Worker

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# 展開された最新版の正しいディレクトリの中にあるVOICEVOX Engineをバックグラウンドで起動
-./voicevox_engine-linux-x64-cpu-0.24.1/run --host 0.0.0.0 &
+# 解凍後に作成される正しいディレクトリパスを指定してVOICEVOX Engineをバックグラウンドで起動
+./voicevox_engine-linux-x64-cpu-0.24.1/voicevox_engine/run --host 0.0.0.0 &
 
 # Discord Botをフォアグラウンドで起動
 python bot.py
