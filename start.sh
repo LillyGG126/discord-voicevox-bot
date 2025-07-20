0d942ccc866a91925c8f198ed8c56ec92d27efba
LillyGG126 <riverslily0126.x@gmail.com>
2025-07-20 15:07:28 +0900

    run file path fixed

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # あなたが特定してくれた正しいパスを指定してVOICEVOX Engineをバックグラウンドで起動
-./voicevox_engine-linux-cpu-x64-0.24.1/linux-cpu-x64/run --host 0.0.0.0 &
+./linux-cpu-x64/run --host 0.0.0.0 &
 
 # Webサーバーをバックグラウンドで起動
 python keep_alive.py &
